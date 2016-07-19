6fdec03d0478cf92501cebc915c92fbaf4ad5ad1
Tim <timcollins_2@hotmail.com>
2016-07-19 17:32:54 -0400

    simplified execution and renamed stuff.

--- WarCraft/WarCraft/WarCraft.cpp
+++ WarCraft/WarCraft/WarCraft.cpp
@@ -3,33 +3,14 @@
 #include "stdafx.h"
 #include "WarCraft.h"
 
-#define MAX_LOADSTRING 100
-
-// Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
-
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_     HINSTANCE hInstance,     //argv[0]
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_	   LPWSTR    lpCmdLine,     //argv[:] all args
+                      _In_	   int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance); // I don't carte who opened me.
+    UNREFERENCED_PARAMETER(lpCmdLine);	   // argv's. None used ATM.
 
-    // TODO: Place code here.
-	std::cout << "OUT!" << std::endl;
-
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WARCRAFT, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
     // Perform application initialization:
@@ -41,7 +22,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WARCRAFT));
 
     MSG msg;
-
+	
     // Main message loop:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
@@ -51,17 +32,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             DispatchMessage(&msg);
         }
     }
-
-    return (int) msg.wParam;
+	
+	return (int)msg.wParam;
 }
 
-
-
-//
 //  FUNCTION: MyRegisterClass()
-//
 //  PURPOSE: Registers the window class.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -69,7 +45,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.cbSize = sizeof(WNDCLASSEX);
 
     wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
+    wcex.lpfnWndProc    = WindowEventHandler;
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
     wcex.hInstance      = hInstance;
@@ -83,16 +59,11 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
-//
 //   FUNCTION: InitInstance(HINSTANCE, int)
-//
 //   PURPOSE: Saves instance handle and creates main window
-//
 //   COMMENTS:
-//
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
@@ -111,20 +82,44 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
 //  PURPOSE:  Processes messages for the main window.
 //
 //  WM_COMMAND  - process the application menu
 //  WM_PAINT    - Paint the main window
 //  WM_DESTROY  - post a quit message and return
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WindowEventHandler(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+	case WM_CREATE:
+		{
+		HWND hWndButton = CreateWindowEx(NULL,
+			L"BUTTON",
+			L"OK",
+			WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+			50,
+			220,
+			100,
+			24,
+			hWnd,
+			(HMENU)MY_BUTTON,
+			GetModuleHandle(NULL),
+			ButtonPress);
+		HWND hEdit = CreateWindowEx(WS_EX_CLIENTEDGE,
+			L"EDIT",
+			L"default msg",
+			WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
+			50,
+			100,
+			200,
+			100,
+			hWnd,
+			(HMENU)MY_STRING,
+			GetModuleHandle(NULL),
+			NULL);
+		}
+		break;
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -157,6 +152,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
+}
+
+LRESULT CALLBACK ButtonPress(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+
 }
 
 // Message handler for about box.
--- WarCraft/WarCraft/WarCraft.h
+++ WarCraft/WarCraft/WarCraft.h
@@ -2,3 +2,27 @@
 
 #include "resource.h"
 #include <iostream>
+
+#define MAX_LOADSTRING 100
+#define MY_BUTTON 101
+#define MY_STRING 102
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING] = L"WarCraft";                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING] = L"WarCraft";            // the main window class name
+
+int APIENTRY wWinMain(_In_     HINSTANCE hInstance,     //argv[0]
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_	   LPWSTR    lpCmdLine,     //argv[:] all args
+	_In_	   int       nCmdShow);
+
+ATOM MyRegisterClass(HINSTANCE hInstance);
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow);
+
+LRESULT CALLBACK WindowEventHandler(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+INT_PTR CALLBACK About	(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
+LRESULT CALLBACK ButtonPress(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
